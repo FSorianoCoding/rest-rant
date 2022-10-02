e197cd6ae87349ecd423d53c670927a6f42b2f0b
Fabian Soriano <fabo.soriano@gmail.com>
2022-10-01 19:14:53 -0700

    styled New and Edit forms

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -8,25 +8,25 @@ function new_form() {
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
                     <div className='row'>
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="name">Place Name</label>
                             <input className="form-control" id="name" name="name" required />
                         </div>
-                        <div className="form-group col-sm-6">
+                        <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label htmlFor="pic">Place Picture</label>
                             <input className="form-control" id="pic" name="pic" />
                         </div>
                     </div>
                     <div className='row'>
-                        <div className="form-group col-sm-4">
+                        <div className="form-group col-sm-4 col-md-3 col-lg-2">
                             <label htmlFor="city">City</label>
                             <input className="form-control" id="city" name="city" />
                         </div>
-                        <div className="form-group col-sm-4">
+                        <div className="form-group col-sm-4 col-md-3 col-lg-2">
                             <label htmlFor="state">State</label>
                             <input className="form-control" id="state" name="state" />
                         </div>
-                        <div className="form-group col-sm-4">
+                        <div className="form-group col-sm-4 col-md-3 col-lg-2">
                             <label htmlFor="founded">Founded Year</label>
                             <input className="form-control" id="founded" name="founded" />
                         </div>
