0858772b3aca1730a7dcc79cea5477adb2177dce
Fabian Soriano <fabo.soriano@gmail.com>
2022-10-08 18:15:05 -0700

    added comments to show page

--- controllers/places.js
+++ controllers/places.js
@@ -26,8 +26,7 @@ router.post("/", (req, res) => {
         }
         console.log("Validation error message", message);
         res.render("places/new", { message });
-      } 
-      else {
+      } else {
         res.render("error404");
       }
     });
@@ -37,18 +36,18 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
-      console.log(place.comments)
-      res.render('places/show', { place })
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
+    .populate("comments")
+    .then((place) => {
+      console.log(place.comments);
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
 router.put("/:id", (req, res) => {
   res.send("PUT /places/:id stub");
@@ -65,6 +64,27 @@ router.get("/:id/edit", (req, res) => {
 router.post("/:id/rant", (req, res) => {
   res.send("GET /places/:id/rant stub");
 });
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
 
 router.delete("/:id/rant/:rantId", (req, res) => {
   res.send("GET /places/:id/rant/:rantId stub");
